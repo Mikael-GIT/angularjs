d010ade1b93a7065358e8ba55be67f136d12889b
Mikael Tavares <mikael.tavares@unigranrio.br>
2023-04-10 20:34:19 -0300

    Html com controladores

--- calculadoradelta/controladores.js
+++ calculadoradelta/controladores.js
@@ -3,6 +3,8 @@ app.controller("controladorDelta",
 		$scope.calcularDelta = function(){
 
             $scope.delta = Math.pow($scope.b, 2) - 4 * $scope.a * $scope.c;
+            $scope.x1 = ((-$scope.b + Math.sqrt($scope.delta)) / 2 * $scope.a).toFixed(2);
+            $scope.x2 = ((-$scope.b - Math.sqrt($scope.delta)) / 2 * $scope.a).toFixed(2);
         }
 	}
 );
